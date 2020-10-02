dcee6c87150001a9bc7a51fb0580ec0757b9dbe2
fasliya <fazy.hfb@gmail.com>
2020-10-02 17:51:24 +0530

    Changes in News

--- news.php
+++ news.php
@@ -55,6 +55,8 @@
                         <p style="text-align: left;">A new national media campaign is asking children and parents to follow seven new and essential ‘lessons’ to limit the risk and spread of COVID-19, as schools reopen across Sri Lanka following months of closure.</p>
                     </div>
 
+
+
                 </form>
 
                 <div style="text-align: center; background-color: #ffffff00" class="latest__blog__text">
@@ -71,45 +73,30 @@
        -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
             box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
                     <div class="filter__form__item filter__form__item--search">
-
-
-
-                        <div class="kt-widget14__header kt-margin-b-30">
-                            <h3 class="kt-widget14__title">
-                                Latest News
-                            </h3>
-                            <span class="kt-widget14__desc">
-														  <p style="font-size: small; margin-top: 0%;"><i style="margin-right: 2%;" class="fa fa-clock-o"></i> 10.00 A.M. - 08 September 2020</p>
-                       									</span>
-                        </div>
-
-
-
                         <div class="filter__form__input">
-                            <img src="img/d.jpg">
+                            <img width="120px" height="120px" class="img_small" src="img/school-students.jpg" style="margin: -20px">
                         </div>
                     </div>
-                    <div class="filter__form__item col-5">
-                        <p style="text-align: right;"><a href="#" class="primary-btn">Read More ></a></p>
 
+                    <div class="filter__form__item col-8">
 
                         <div class="kt-widget14__header kt-margin-b-30">
-                            <h3 style="font-weight: 400; margin-bottom: -4%;" class="kt-widget14__title">
+                            <h3 style="font-weight: 400; margin: -8%; margin-left: -18%" class="kt-widget14__title">
                                 Covid-19 : Back to School
                             </h3>
 
+
+                        </div>
+                        <p style="text-align: left; margin-left: -18%">A new nationad parents to follow</p>
+
+                        <div style="margin-bottom:-20%; margin-top: -20%; margin-right: -101%; background-color: #ffffff00" class="latest__blog__text">
+                            <a style="font-size: x-large; " href="#">Read More</a>
                         </div>
 
-
-
-                        <p style="text-align: left;">A new national media campaign is asking children and parents to follow seven new and essential ‘lessons’ to limit the risk and spread of COVID-19, as schools reopen across Sri Lanka following months of closure.</p>
-                    </div>
+                        </div>
 
                 </form>
 
-                <div style="text-align: center; background-color: #ffffff00" class="latest__blog__text">
-                    <a style="font-size: x-large; " href="#">Older News</a>
-                </div>
                 <br><br>
             </div>
 
