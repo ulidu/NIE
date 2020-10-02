51fedbe1565093a9adc14958196a7d2f3ee24be7
fasliya <fazy.hfb@gmail.com>
2020-10-02 07:48:27 +0530

    Implemented Latest News Portlet

---
+++ news.php
@@ -0,0 +1,50 @@
+<?php include "includes/header.html"; ?>
+
+<!-- Content Begin -->
+
+<section class="latest-blog spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div style="text-align: left" class="section-title">
+                    <h2>WHAT'S NEW FROM NIE</h2>
+                    <h5>Home > News</h5>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+
+                <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.3), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <p style="font-size: x-large">Latest News</p>
+                        <p style="font-size: small; margin-top: -8%;"><i style="margin-right: 2%;" class="fa fa-clock-o"></i> 10.00 A.M. - 08 September 2020</p>
+                        <div class="filter__form__input">
+                            <img src="img/d.jpg">
+                        </div>
+                    </div>
+                    <div class="filter__form__item col-5">
+                        <p style="text-align: right;"><a href="#" class="primary-btn">Read More ></a></p>
+                        <p style="text-align: left; font-size: xx-large; font-weight: 800; font-family: 'Playfair Display', serif; line-height: normal;">Covid-19 : Back to School</p>
+
+                        <p style="text-align: left;">A new national media campaign is asking children and parents to follow seven new and essential ‘lessons’ to limit the risk and spread of COVID-19, as schools reopen across Sri Lanka following months of closure.</p>
+                    </div>
+
+                </form>
+
+                <div style="text-align: center; background-color: #ffffff" class="latest__blog__text">
+                    <a style="font-size: x-large; " href="#">Older News</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Content End -->
+
+<?php include "includes/footer.html"; ?>
