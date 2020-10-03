c8596519f0536746390c6a84340fbfa29775e487
fasliya <fazy.hfb@gmail.com>
2020-10-03 12:43:14 +0530

    Implementation of Notices

--- notices.php
+++ notices.php
@@ -2,9 +2,299 @@
 
 <!-- Content Begin -->
 
+<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div style="text-align: left" class="section-title">
+                    <br><br><br>
+                    <h2>Notices and Events</h2>
+                    <h5>Home > Notices and Events</h5>
+                </div>
+            </div>
+        </div>
+
+        <h2 style="text-align: center">Special Notices</h2><br><br>
+
+        <div class="row">
+            <div class="col-6">
+
+                <form style="border-width: 3px; border-style: solid; border-color: transparent transparent transparent #E9AD28; position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/meeting11.jpg"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                Annual Staff Awareness
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;">NIE Staff Awareness Program for Security 07th November, 2020.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+
+                <br>
+
+                <form style="border-width: 3px; border-style: solid; border-color: transparent transparent transparent #E9AD28; "position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/degree.jpg"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                Closing Dates Extended
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;">Open University extends the closing date for Bachelor of Science degree.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+
+                <br>
+
+
+                <form style="border-width: 3px; border-style: solid; border-color: transparent transparent transparent #E9AD28; "position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/res.jpg"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                Publicashion of SAARC
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;">Reopened schools after 4 months due to lockdown in Sri Lanka.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
 
 
 
+
+            </div>
+
+
+            <div class="col-6">
+
+                <form style="border-width: 3px; border-style: solid; border-color: transparent transparent transparent #E9AD28; "position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/train.jpg"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                iTESL : Teacher Education
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;">The Closing date for the post of lead trainer in ITESL project has been extended.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+
+                <br>
+
+
+                <form style="border-width: 3px; border-style: solid; border-color: transparent transparent transparent #E9AD28; "position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/prize.jpg"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                Preparation of Audio materials
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;">Preparation of Audio materials for Primary and Secondary Levels in schools.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+
+                <br>
+
+
+                <form style="border-width: 3px; border-style: solid; border-color: transparent transparent transparent #E9AD28; "position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/cert.jpg"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                PRINSETT  Final Certificates
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;"> Distributed by the Dept of English, NIE will now be handled by the Dept. of Examinations.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+
+            </div>
+
+
+        </div>
+
+<br><br>
+       <!-- <a href="#" class="primary-btn" style="margin-right: 15%">Read More ></a>-->
+
+        <section class="latest-blog spad">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="section-title" >
+                            <h5>NEWS & EVENT</h5>
+                            <h2>From NIE</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-3 p-0 order-lg-1 col-md-6 order-md-1">
+                        <div class="latest__blog__pic set-bg" data-setbg="img/bakthi.jpg"></div>
+                    </div>
+                    <div class="col-lg-3 p-0 order-lg-2 col-md-6 order-md-2">
+                        <div class="latest__blog__text">
+                            <div class="label">Religious</div>
+                            <h5>Bhakthi Gee Program at the Faculty of ELDM Meepe</h5>
+                            <p><i class="fa fa-clock-o"></i> 19th March, 2020</p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 p-0 order-lg-3 col-md-6 order-md-4">
+                        <div class="latest__blog__pic set-bg" data-setbg="img/educ.jpg"></div>
+                    </div>
+                    <div class="col-lg-3 p-0 order-lg-4 col-md-6 order-md-3">
+                        <div class="latest__blog__text">
+                            <div class="label">Diploma Awards</div>
+                            <h5>Diploma Award Ceramony - NCOE -2019 at Temple Trees</h5>
+                            <p><i class="fa fa-clock-o"></i> 20th August, 2019</p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 p-0 order-lg-6 col-md-6 order-md-5">
+                        <div class="latest__blog__pic latest__blog__pic__last__row set-bg"
+                             data-setbg="img/charity.jpg"></div>
+                    </div>
+                    <div class="col-lg-3 p-0 order-lg-5 col-md-6 order-md-6">
+                        <div class="latest__blog__text">
+                            <div class="label">Charity</div>
+                            <h5>Almsgiving Ceremoney held by Transport Unit</h5>
+                            <p><i class="fa fa-clock-o"></i> 01st January, 2020</p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 p-0 order-lg-8 col-md-6 order-md-8">
+                        <div class="latest__blog__pic latest__blog__pic__last__row set-bg"
+                             data-setbg="img/grad.jpg"></div>
+                    </div>
+                    <div class="col-lg-3 p-0 order-lg-7 col-md-6 order-md-7">
+                        <div class="latest__blog__text">
+                            <div class="label">Graduation</div>
+                            <h5>Annual NIE Convocation 2019 at BMICH</h5>
+                            <p><i class="fa fa-clock-o"></i> 29th March, 2020</p>
+                            <a href="#">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <br><br><br><br>
+    </div>
+</div>
 
 <!-- Content End -->
 
