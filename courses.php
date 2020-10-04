8d5867eda307c3a025e120be1aa7c5301292151b
fasliya <fazy.hfb@gmail.com>
2020-10-04 08:41:18 +0530

    Implementing Notices Section

---
+++ courses.php
@@ -0,0 +1,336 @@
+<?php include "includes/header.html"; ?>
+
+<!-- Content Begin -->
+
+<div class="kt-container  kt-container--fluid  kt-grid__item kt-grid__item--fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div style="text-align: left" class="section-title">
+                    <br><br><br>
+                    <h2>NIE COURSES</h2>
+                    <h5>Home > Courses</h5>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+
+            <div class="col-lg-12">
+                <div style="text-align: left" class="section-title">
+                    <br><br><br>
+                    <h2 style="font-size: ">NIE COURSES</h2>
+
+                </div>
+            </div>
+
+            <div class="col-6">
+
+                <form style=" position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+
+
+                        <div class="kt-widget14__header kt-margin-b-30">
+                            <h3 class="kt-widget14__title">
+                                Latest News
+                            </h3>
+                            <span class="kt-widget14__desc">
+														  <p style="font-size: small; margin-top: 0%;"><i
+                                                                      style="margin-right: 2%;"
+                                                                      class="fa fa-clock-o"></i> 10.00 A.M. - 08 September 2020</p>
+                       									</span>
+                        </div>
+
+
+                        <div class="filter__form__input">
+                            <img src="img/d.jpg">
+                        </div>
+                    </div>
+                    <div class="filter__form__item col-5">
+                        <p style="text-align: right;"><a href="#" class="primary-btn">Read More ></a></p>
+
+
+                        <div class="kt-widget14__header kt-margin-b-30">
+                            <h3 style="font-weight: 400; margin-bottom: -4%;" class="kt-widget14__title">
+                                Covid-19 : Back to School
+                            </h3>
+
+                        </div>
+
+
+                        <p style="text-align: left;">A new national media campaign is asking children and parents to
+                            follow seven new and essential ‘lessons’ to limit the risk and spread of COVID-19, as
+                            schools reopen across Sri Lanka following months of closure.</p>
+                    </div>
+
+
+                </form>
+
+                <div style="text-align: center; background-color: #ffffff00" class="latest__blog__text">
+                    <a style="font-size: x-large; " href="#">Older News</a>
+                </div>
+                <br><br>
+            </div>
+
+
+            <div class="col-6">
+
+                <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/school-students.jpg"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                Grade V Scholarship
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;">Fifteen minutes extra will be provided for Grade V Scholarship Examination on October.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+
+                <br>
+
+
+                <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/postgraduate-01-mobile.jpg"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                Post Graduate Diploma
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;">Registrations open for new intake of Post Graduate Diploma in Education.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+
+                <br>
+
+
+                <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
+                    <div class="filter__form__item filter__form__item--search">
+                        <div class="filter__form__input">
+                            <img width="120px" height="120px" class="" src="img/smart.png"
+                                 style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                        </div>
+                    </div>
+
+                    <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                        <div class="kt-widget14__header">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                Schools reopen in Sri Lanka
+                            </h3>
+
+
+                            <p style="text-align: left; font-size: 15px;">Reopened schools after 4 months due to lockdown in Sri Lanka.</p>
+
+                            <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                 class="latest__blog__text">
+                                <a style="font-size: large;" href="#">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+
+            </div>
+
+
+        </div>
+            <div style="margin-bottom: 1%; margin-top:-2%; right:0; width: 80vw;" class="ticker-wrapper">
+                <div class="bigHeading">News Line</div>
+                <div class="text-update">
+                    <p><span class="dot"></span> USA extends support to develop Sri Lanka’s education system. &nbsp;&nbsp;
+
+
+                    <span class="dot"></span> Academic activities at schools as usual.&nbsp;&nbsp;
+
+
+
+                    </p>
+                </div>
+            </div>
+
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div style="text-align: left" class="section-title">
+                    <br><br><br>
+                    <h2>SPECIAL NEWS</h2>
+                    <h5>Special News Related to Education</h5>
+                </div>
+            </div>
+        </div>
+        <div class="gallery__slider owl-carousel">
+            <div class="gallery__item small__item set-bg" data-setbg="img/gallery/sp1.jpg">
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+                <form action="#" class="filter__form">
+
+                    <div style="text-align: center; width: 84%;" class="filter__form__item">
+
+                        <div class="kt-widget14__header">
+                            <hr class="new4">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+
+                                Prevent Covid-19: Stay Healthy
+                            </h3>
+
+<br>
+                            <p style="text-align: center; font-size: 15px;">Cover your nose and mouth with a face mask when schooling</p>
+
+                            <hr class="new4">
+                        </div>
+                    </div>
+
+                </form>
+
+
+            </div>
+            <div class="gallery__item set-bg" data-setbg="img/gallery/sp2.jpg">
+
+
+
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+                <form action="#" class="filter__form">
+
+                    <div style="text-align: center;width: 84%;" class="filter__form__item">
+
+                        <div class="kt-widget14__header">
+                            <hr class="new4">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+
+                                Greetings : Children's Day
+                            </h3>
+
+                            <br>
+                            <p style="text-align: center; font-size: 15px;">Message by Prime Minister Mahinda on the World Children’s Day</p>
+
+                            <hr class="new4">
+                        </div>
+                    </div>
+
+                </form>
+
+
+
+
+            </div>
+            <div class="gallery__item set-bg" data-setbg="img/gallery/sp4.jpg">
+
+
+
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+                <form action="#" class="filter__form">
+
+                    <div style="text-align: center; width: 84%;" class="filter__form__item">
+
+                        <div class="kt-widget14__header">
+                            <hr class="new4">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+
+                                IDs for NCE professionals
+                            </h3>
+
+                            <br>
+                            <p style="text-align: center; font-size: 15px;">Issue Identity Cards to principals,lecturers and teachers in NCE</p>
+
+                            <hr class="new4">
+                        </div>
+                    </div>
+
+                </form>
+
+
+
+
+            </div>
+            <div class="gallery__item set-bg">
+                <video style="position: absolute; z-index: -10000000000; width: 100%;" autoplay="autoplay" muted="muted" loop="loop">
+                    <source src="img/gallery/vd.mp4" type="video/mp4">
+                </video>
+
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+                <form style="z-index: +100000000000000000;position: absolute; width: 100%" action="#" class="filter__form">
+
+                    <div style="text-align: center; width: 84%;" class="filter__form__item">
+
+                        <div class="kt-widget14__header">
+                            <hr class="new4">
+                            <h3 style="font-weight: 400;" class="kt-widget14__title">
+
+                                Discussions : Raise on Forum
+                            </h3>
+
+                            <br>
+                            <p style="text-align: center; font-size: 15px;">Weekly discussion forum on curriculum reforms was held on 12/10/2020</p>
+
+
+
+
+
+                            <hr class="new4">
+                        </div>
+                    </div>
+
+                </form>
+
+
+
+            </div>
+        </div>
+    <br><br><br><br>
+    </div>
+</div>
+
+<!-- Content End -->
+
+<?php include "includes/footer.html"; ?>
