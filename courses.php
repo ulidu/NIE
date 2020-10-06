8a268758ffbe0c57e2f700e9044e035f4679a411
ulidu <ulidutheerake@gmail.com>
2020-10-06 13:27:21 +0530

    Implementing Courses Section

--- courses.php
+++ courses.php
@@ -18,34 +18,318 @@
         <div class="row">
             <div class="col-12">
 
-                <form style="position:relative;
-    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
-       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
-            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#" class="filter__form">
-                    <div class="filter__form__item filter__form__item--search col-12">
+                <div class="row">
 
-                        <div class="row">
-                        <div class="col-3">
-                        <img width="100%" src="img/courses/">
-                        </div>
-                            <div class="col-9">
-                        <p>Followings are the courses that are provided by National
+
+                    <div class="col-7">
+
+                        <h2 style="color: #E9AD28;">Discover the Right Course for you.</h2>
+                        <br>
+                        <h4 style="color: #4D5656; font-weight: 500;">Followings are the courses that are provided by
+                            National
                             Institute of Education. You can download relevant
-                            application to your preferred course. Click on course to get started.</p>
-                            </div>
-                        </div>
+                            application to your preferred course. Click on View Courses to get started.</h4>
+                        <br><br>
+                        <br>
+                        <a href="#course1" style="font-family: 'Poppins',sans-serif; font-size: large;"
+                           class="primary-btn">View Courses</a>
+
+
                     </div>
 
+                    <div class="col-5">
+                        <img width="100%" src="img/courses/Asset.png">
+                    </div>
 
+                </div>
 
-                </form>
-<br><br>
+                <br><br>
 
             </div>
         </div>
 
 
+        <section id="course1">
 
+
+            <br>
+
+            <h2 style="font-weight: 400;">Most Popular Courses offered by NIE</h2>
+            <br><br>
+
+            <div class="row">
+                <div class="col-6">
+
+                    <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#"
+                          class="filter__form hover-div">
+                        <div class="filter__form__item filter__form__item--search">
+                            <div class="filter__form__input">
+                                <img width="120px" height="120px" class="" src="img/meeting11.jpg"
+                                     style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                            </div>
+                        </div>
+
+                        <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                            <div class="kt-widget14__header">
+                                <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                    Annual Staff Awareness
+                                </h3>
+
+
+                                <p style="text-align: left; font-size: 15px;">NIE Staff Awareness Program for Security
+                                    07th
+                                    November, 2020.</p>
+
+                                <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                     class="latest__blog__text">
+                                    <a style="font-size: large;" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+                    <br>
+
+                    <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#"
+                          class="filter__form hover-div">
+                        <div class="filter__form__item filter__form__item--search">
+                            <div class="filter__form__input">
+                                <img width="120px" height="120px" class="" src="img/deg.jpg"
+                                     style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                            </div>
+                        </div>
+
+                        <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                            <div class="kt-widget14__header">
+                                <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                    Closing Dates Extended
+                                </h3>
+
+
+                                <p style="text-align: left; font-size: 15px;">Open University extends the closing date
+                                    for
+                                    Bachelor of Science degree.</p>
+
+                                <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                     class="latest__blog__text">
+                                    <a style="font-size: large;" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+                    <br>
+
+
+                    <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#"
+                          class="filter__form hover-div">
+                        <div class="filter__form__item filter__form__item--search">
+                            <div class="filter__form__input">
+                                <img width="120px" height="120px" class="" src="img/res.jpg"
+                                     style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                            </div>
+                        </div>
+
+                        <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                            <div class="kt-widget14__header">
+                                <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                    Publication of SAARC
+                                </h3>
+
+
+                                <p style="text-align: left; font-size: 15px;">Reopened schools after 4 months due to
+                                    lockdown in Sri Lanka.</p>
+
+                                <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                     class="latest__blog__text">
+                                    <a style="font-size: large;" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+                </div>
+
+
+                <div class="col-6">
+
+                    <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#"
+                          class="filter__form hover-div">
+                        <div class="filter__form__item filter__form__item--search">
+                            <div class="filter__form__input">
+                                <img width="120px" height="120px" class="" src="img/train.jpg"
+                                     style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                            </div>
+                        </div>
+
+                        <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                            <div class="kt-widget14__header">
+                                <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                    ITESL : Teacher Education
+                                </h3>
+
+
+                                <p style="text-align: left; font-size: 15px;">The Closing date for the post of lead
+                                    trainer
+                                    in ITESL project has been extended.</p>
+
+                                <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                     class="latest__blog__text">
+                                    <a style="font-size: large;" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+                    <br>
+
+
+                    <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#"
+                          class="filter__form hover-div">
+                        <div class="filter__form__item filter__form__item--search">
+                            <div class="filter__form__input">
+                                <img width="120px" height="120px" class="" src="img/prize.jpg"
+                                     style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                            </div>
+                        </div>
+
+                        <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                            <div class="kt-widget14__header">
+                                <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                    Preparation of Audio materials
+                                </h3>
+
+
+                                <p style="text-align: left; font-size: 15px;">Preparation of Audio materials for Primary
+                                    and
+                                    Secondary Levels in schools.</p>
+
+                                <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                     class="latest__blog__text">
+                                    <a style="font-size: large;" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+                    <br>
+
+
+                    <form style="position:relative;
+    -webkit-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+       -moz-box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;
+            box-shadow:0 1px 4px rgba(0, 0, 0, 0.1), 0 0 40px rgba(0, 0, 0, 0.1) ;" action="#"
+                          class="filter__form hover-div">
+                        <div class="filter__form__item filter__form__item--search">
+                            <div class="filter__form__input">
+                                <img width="120px" height="120px" class="" src="img/cert.jpg"
+                                     style="margin-left: -19px; margin-top: -4%; margin-bottom: -4%;">
+                            </div>
+                        </div>
+
+                        <div style="margin-top: -3%; margin-left: -30%;" class="filter__form__item col-8">
+
+                            <div class="kt-widget14__header">
+                                <h3 style="font-weight: 400;" class="kt-widget14__title">
+                                    PRINSETT Final Certificates
+                                </h3>
+
+
+                                <p style="text-align: left; font-size: 15px;"> Distributed by the Dept of English, NIE
+                                    will
+                                    now be handled by the Dept. of Examinations.</p>
+
+                                <div style="background-color: #ffffff00; margin-left: -9%; margin-bottom: -10%; margin-top: -15%;"
+                                     class="latest__blog__text">
+                                    <a style="font-size: large;" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+                </div>
+
+
+            </div>
+            <br><br>
+            <center><img src="img/notices/arr.gif" width="6%;"></center>
+
+            <br><br>
+        </section>
+
+
+        <div class="row">
+            <div class="col-12">
+
+                <div class="row">
+
+                    <div class="col-5">
+                        <img width="100%" src="img/courses/ab.png">
+                    </div>
+
+                    <div style="text-align: right;" class="col-7">
+
+                        <h2 style="color: #E9AD28;">For all Teachers and Teacher Educators.</h2>
+                        <br>
+
+                        <h4 style="color: #4D5656; font-weight: 500;">Following table displays all the courses that are offered by
+                            National Institute of Education for all teachers and Teacher Educators. You can download relevant
+                            application to your preferred course by clicking <span style="color: #E9AD28;">Download Application </span>button right next to the particular corse.</h4>
+                        <br>
+
+
+
+                    </div>
+
+
+
+                </div>
+
+                <br><br>
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
 
     </div>
 </div>
